0810be2bd7270e5037bd7f54315c3fa8e5e6a81b
ygu2009 <ying.y.gu@gmail.com>
2015-12-14 00:59:47 -0600

    2015-12-13

--- freetext2jason.py
+++ freetext2jason.py
@@ -31,13 +31,31 @@ ref_url=[]
 nn=-1
 with open("proc.txt", "r") as proc_file:
     for line in proc_file:
-        name_pattern='[A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z]\. [A-Z][a-z]+|et al\.|etc\.'
-        nn=nn+1
-        ref_author.append([])
-        for match in re.findall(name_pattern, line):
-            ref_author[nn].append(match)
-            # print(match)
+        ref_flag=0 #detect the correct format refrence for later process
+        ref_idx=[]
+        pattern_idx=re.compile('\[[0-9]*\]')
+        match_idx = re.search(pattern_idx,line)
+        pattern_author=re.compile('\s[A-Z]\. [A-Z][a-z]+|\s[A-Z] [A-Z][a-z]+|\s[A-Z]\. [A-Z]\. [A-Z][a-z]+|\set al\.|\setc\.')
+        match_author = re.search(pattern_author, line)
+        if match_idx and match_author:
+            ref_flag=1
+        # print line
+        if ref_flag==1:
+            nn=nn+1
+            ref_author.append([])
+            for match in pattern_author.finditer(line):
+                ref_author[nn].append(match.group().strip(' '))
+                pos_end=match.end()
+            # print ref_author[nn]
+            # print pos_end
+
+            pattern_title=re.compile('\\"(.+)\\"') #assuming that the title is in ""
+            for match in pattern_title.findall(line):
+                print match
+            # print(pattern_title[1])
+
+
 
     # print(ref_idx)
     # print(ref_txt)
-    print(ref_author)
+    # print(ref_author[0])
